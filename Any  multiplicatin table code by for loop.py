9c359642d7aacad76dde3fc5204f67ce92762eda
sahil300200 <146620795+sahil300200@users.noreply.github.com>
2024-02-24 16:33:32 +0530

    Add files via upload

---
+++ Any  multiplicatin table code by for loop.py
@@ -0,0 +1,12 @@
+# -*- coding:-*-
+"""
+Created on Thu Sep 21 23:09:39 2023
+
+@author: admin
+"""
+
+num = int(input("enter a number:"))
+
+for a in range(1,11):
+    print(num,'x',a,'=',num*a)
+    
